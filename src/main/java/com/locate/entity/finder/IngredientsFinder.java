16f20957a8dcb32436dae23f99563e515c47f96d
binaymishramca@gmail.com <binaymishramca@gmail.com>
2018-02-22 22:31:30 +0530

    Refactoring

--- src/main/java/com/locate/entity/finder/IngredientsFinder.java
+++ src/main/java/com/locate/entity/finder/IngredientsFinder.java
@@ -1,19 +1,7 @@
 package com.locate.entity.finder;
 
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.InputStream;
-
-import opennlp.tools.namefind.NameFinderME;
-import opennlp.tools.namefind.NameSample;
-import opennlp.tools.namefind.NameSampleDataStream;
-import opennlp.tools.namefind.TokenNameFinderFactory;
-import opennlp.tools.namefind.TokenNameFinderModel;
-import opennlp.tools.util.MarkableFileInputStreamFactory;
-import opennlp.tools.util.ObjectStream;
-import opennlp.tools.util.PlainTextByLineStream;
-import opennlp.tools.util.TrainingParameters;
 
 public class IngredientsFinder extends NamedEntityFinder{
 	
@@ -21,26 +9,8 @@ public class IngredientsFinder extends NamedEntityFinder{
 		super(model);
 	}
 	
-
 	public static void train(File trainingFile, File modelFile) throws Exception {
-
-	    ObjectStream<String> in = new PlainTextByLineStream(new MarkableFileInputStreamFactory(trainingFile), charset);
-	    ObjectStream<NameSample> samples = new NameSampleDataStream(in);
-	    TokenNameFinderModel model;
-	    try{
-	      model = NameFinderME.train("en", "ingredient", samples, TrainingParameters.defaultParams(),new TokenNameFinderFactory());
-	    }finally {
-	      samples.close();
-	      in.close();
-	    }
-
-	    BufferedOutputStream modelOut = new BufferedOutputStream(new FileOutputStream(modelFile, true));
-	    try{
-	      model.serialize(modelOut);
-	    }finally {
-	      modelOut.flush();
-	      modelOut.close();
-	    }
+		NamedEntityFinder.train(trainingFile, modelFile);
 	}
 
 }
--- src/main/java/com/locate/entity/finder/NamedEntityFinder.java
+++ src/main/java/com/locate/entity/finder/NamedEntityFinder.java
@@ -1,13 +1,23 @@
 package com.locate.entity.finder;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
 import opennlp.tools.namefind.NameFinderME;
+import opennlp.tools.namefind.NameSample;
+import opennlp.tools.namefind.NameSampleDataStream;
+import opennlp.tools.namefind.TokenNameFinderFactory;
 import opennlp.tools.namefind.TokenNameFinderModel;
+import opennlp.tools.util.MarkableFileInputStreamFactory;
+import opennlp.tools.util.ObjectStream;
+import opennlp.tools.util.PlainTextByLineStream;
 import opennlp.tools.util.Span;
+import opennlp.tools.util.TrainingParameters;
 
 public class NamedEntityFinder extends NameFinderME implements EntityFinder<String[], List<String>>{
 	
@@ -24,5 +34,28 @@ public class NamedEntityFinder extends NameFinderME implements EntityFinder<Stri
 		for(String entity: Span.spansToStrings(spans, tokens))
 			entities.add(entity);
 		return entities;
+	}
+	
+	
+
+	public static void train(File trainingFile, File modelFile) throws Exception {
+
+	    ObjectStream<String> in = new PlainTextByLineStream(new MarkableFileInputStreamFactory(trainingFile), charset);
+	    ObjectStream<NameSample> samples = new NameSampleDataStream(in);
+	    TokenNameFinderModel model;
+	    try{
+	      model = NameFinderME.train("en", "ingredient", samples, TrainingParameters.defaultParams(),new TokenNameFinderFactory());
+	    }finally {
+	      samples.close();
+	      in.close();
+	    }
+
+	    BufferedOutputStream modelOut = new BufferedOutputStream(new FileOutputStream(modelFile, true));
+	    try{
+	      model.serialize(modelOut);
+	    }finally {
+	      modelOut.flush();
+	      modelOut.close();
+	    }
 	}
 }
